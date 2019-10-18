d6d595be9e6e171ecc66475f76cb5ce073f2f187
Julien Fortin <julienstpierrefortin@gmail.com>
2019-10-18 09:28:16 -0400

    Update port reference

--- manage.py
+++ manage.py
@@ -19,7 +19,7 @@ manager = Manager(app)
 @manager.command
 def run():
 	port = os.getenv('PORT', 5000)
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=port)
 
 @manager.command
 def test():
