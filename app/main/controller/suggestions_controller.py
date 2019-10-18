0ace645d39da56f8de85f10c180226b69582168d
nitrofortin <julienstpierrefortin@gmail.com>
2019-10-18 06:39:41 -0400

    Update suggestions controller to use scoring/indexing engine

--- app/main/controller/suggestions_controller.py
+++ app/main/controller/suggestions_controller.py
@@ -1,40 +1,26 @@
-from flask import request
+from flask import request, Response
 from flask_restplus import Resource, Namespace
 
-api = Namespace('suggestions', description='All valid suggestions')
+import unidecode
+import json
 
-mock = {
-  "suggestions": [
-    {
-      "name": "London, ON, Canada",
-      "latitude": "42.98339",
-      "longitude": "-81.23304",
-      "score": 0.9
-    },
-    {
-      "name": "London, OH, USA",
-      "latitude": "39.88645",
-      "longitude": "-83.44825",
-      "score": 0.5
-    },
-    {
-      "name": "London, KY, USA",
-      "latitude": "37.12898",
-      "longitude": "-84.08326",
-      "score": 0.5
-    },
-    {
-      "name": "Londontowne, MD, USA",
-      "latitude": "38.93345",
-      "longitude": "-76.54941",
-      "score": 0.3
-    }
-  ]
-}
+from app.engine.query import Query
+from app.engine.score import ScoreInterface
+
+
+api = Namespace('suggestions', description='All valid suggestions')
 
 @api.route('/')
 class Suggestions(Resource):
     @api.doc('list_of_valid_suggestions')
     def get(self):
-        """List all registered users"""
-        return mock
+        query = Query(request.args)
+        data = ScoreInterface().run(query)
+        response = {
+            'suggestions': data
+        }
+
+        response = json.dumps(response, ensure_ascii=False)
+        response = Response(response, 
+            content_type="application/json; charset=utf-8")
+        return response
