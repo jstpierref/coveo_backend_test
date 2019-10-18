05295f8c61ff39cded07d06dd2849b56fab28190
Julien Fortin <julienstpierrefortin@gmail.com>
2019-10-18 08:18:10 -0400

    Fix tests

--- app/test/test_trie.py
+++ app/test/test_trie.py
@@ -4,7 +4,7 @@ from app.engine.trie import Trie
 class TestIndexing(unittest.TestCase):
 
     def test_trie_add_word(self):
-        trie = trie()
+        trie = Trie()
         trie.add_word(1, "test")
 
         self.assertEqual(list(trie.root.get_children().keys())[0], 't')
@@ -18,13 +18,13 @@ class TestIndexing(unittest.TestCase):
         self.assertEqual(len(trie.root.get_children().keys()), 1)
 
     def test_trie_add_words(self):
-        trie = trie()
+        trie = Trie()
         trie.add_words([(1, "test"), (2, "tester")])
         self.assertEqual(list(trie.root.get_children().keys())[0], 't')
         self.assertEqual(trie.autocomplete("test"), [(1, 'test', None), (2, 'tester', None)])
 
     def test_trie_autocomplete(self):
-        trie = trie()
+        trie = Trie()
         trie.add_word(1, "test")
         trie.add_word(2, "tester")
         trie.add_word(3, "team")
@@ -37,7 +37,7 @@ class TestIndexing(unittest.TestCase):
         self.assertEqual(trie.autocomplete("tested"), [])
 
     def test_trie_duplicates(self):
-        trie = trie()
+        trie = Trie()
         trie.add_word(1, "test")       
         trie.add_word(2, "test")    
         trie.add_word(3, "team")    
