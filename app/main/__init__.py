a6d50015ef1c67ed27251608ac6ac936b2e90500
Julien Fortin <julienstpierrefortin@gmail.com>
2019-10-18 16:22:58 -0400

    Add route for env variables

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from .config import config_table
+import os
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_table[config_name])
+
+	@app.route('/vars')
+	def home():
+	    environment_variables = { key: os.environ[key] for key in os.environ.keys() }
+	    return jsonify(environment_variables)
+
     return app
